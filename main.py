9e362b515b33ef52d7b40d312dda29a8d7d433fb
FakeLads <22166@students.mrgs.school.nz>
2026-04-21 12:35:14 +1200

    created pycharm file imported tkinter

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("Help")
+
+# Imports tkinter features
+from tkinter import *
+
+# Imports the messagebox function from the tkinter library
+from tkinter import messagebox
+
+# Imports PIL/Pillow
+from PIL import Image, ImageTk
+
+from tkinter import PhotoImage
+
+# Imports the 'random' function
+import random
+
+
+main = Tk()
+
+main.mainloop()
