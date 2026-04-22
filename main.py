c72220dc13079b9d1174ec982a801e6af73e9410
FakeLads <22166@students.mrgs.school.nz>
2026-04-23 09:53:18 +1200

    created pycharm file imported tkinter added canvas widgets

--- main.py
+++ main.py
@@ -5,14 +5,40 @@ from tkinter import *
 # Imports the messagebox function from the tkinter library
 from tkinter import messagebox
 
-# Imports PIL/Pillow
-from PIL import Image, ImageTk
-
 from tkinter import PhotoImage
 
-# Imports the 'random' function
-import random
 
+class Homepage:
+    def __init__(self, main_canvas,bg_image_canvas_id):
+
+        self.main_canvas = main_canvas
+
+        self.bg_image_canvas_id =  bg_image_canvas_id
+
+        self.canvas_width = main_canvas.winfo_width()
+        self.canvas_height = main_canvas.winfo_height()
+
+        # If the width and height of an image equates to 1, the size will be changed to the following values below it.
+        if self.canvas_width == 1:
+            self.canvas_width = 1400
+        if self.canvas_height == 1:
+            self.canvas_height == 800
+
+
+        self.inner_frame = Frame(main_canvas, bg="Grey", padx=40, pady=100)
+
+        self.center_x = self.canvas_width / 2
+        self.center_y = self.canvas_height / 2
+
+        self.frame_window = main_canvas.create_window(self.center_x, self.center_y, window=self.inner_frame,
+                                                      anchor="center")
+
+        self.inner_frame.columnconfigure(0, weight=1)
+        self.inner_frame.columnconfigure(1, weight=3)
+        self.inner_frame.columnconfigure(2, weight=1)
+
+        for i in range(4):
+            self.inner_frame.rowconfigure(i, weight=1)
 
 main = Tk()
 
