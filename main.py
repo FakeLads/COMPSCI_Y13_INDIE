b5613579559eb32b6c7d9aa68a389e162e54d6cf
FakeLads <22166@students.mrgs.school.nz>
2026-05-06 11:10:01 +1200

    created pycharm file imported tkinter added canvas widgets fixed added widget code window

--- main.py
+++ main.py
@@ -2,36 +2,58 @@
 # Imports tkinter features
 from tkinter import *
 
-# Imports the messagebox function from the tkinter library
-from tkinter import messagebox
-
 from tkinter import PhotoImage
 
-# The Login page for the GUI program
+root = Tk()
+root.title("Take Credit")
+root.geometry("800x500")
+
 class Loginpage:
     def __init__(self, parent):
 
-        self.parent = parent
+        self.bg = PhotoImage(file="images/login_page_buttons.png")
 
-        self.inner_frame = Frame(parent, bg="Blue", padx=40, pady=100)
+        self.my_canvas = Canvas(root, width=800, height=500)
+        self.my_canvas.pack(fill="both", expand=True)
 
-        self.start_button = Button(self.inner_frame, width = 40, bg="White", fg="Black", font=("Helvetica", "24"), )
-        self.start_button.grid(row=1, column=3, pady=10, padx=10, sticky="nsew")
+        self.my_canvas.create_image(0,0, image=self.bg, anchor="nw")
 
-        self.entry_id = Entry(self.inner_frame, width = 40, bg = "White", fg="Black", font=("Helvetica", "24"), )
-        self.entry_id.grid(row=0, column=2, padx=0, pady=0, sticky="nsew")
+        self.my_canvas.create_text(400,250, text="")
 
-        self.exit_button = Button(self.inner_frame, width = 10, height= 10, bg = "White", fg="Black", font=("Helvetica", "12"), )
-        self.exit_button.grid(row=3, column=3, padx=0, pady=0, sticky="nsew")
+        self.inner_frame = Frame(root, bg="Black", )
+
+        self.label = Label(root, text="Hello, Tkinter!")
+        self.label.pack()
+
+        self.enter_button = Button()
+
+root.mainloop()
 
 
 
 
-if __name__ == "__main__":
 
-    root = Tk()
-    root.title("Take Credit")
-    #root.geometry("1920X1080")
-    #root.resizable(False, False)
-    Loginpage(root)
-    root.mainloop()
+# The Login page for the GUI program
+#class Loginpage:
+ #   def __init__(self, parent):
+#
+ #       self.parent = parent
+#
+ #       self.inner_frame = Frame(parent, bg="Blue", padx=40, pady=100)
+  #      self.inner_frame.pack()
+#
+#
+ #       self.start_button = Button(self.inner_frame, width = 40, bg="White", fg="Black", font=("Helvetica", "24"), )
+#        self.start_button.grid(row=1, column=3, pady=10, padx=10, sticky="nsew")
+
+ #       self.entry_id = Entry(self.inner_frame, width = 40, bg = "White", fg="Black", font=("Helvetica", "24"), )
+  #      self.entry_id.grid(row=0, column=2, padx=0, pady=0, sticky="nsew")
+#
+ #       self.exit_button = Button(self.inner_frame, width = 10, height= 10, bg = "White", fg="Black", font=("Helvetica", "12"), )
+  #      self.exit_button.grid(row=3, column=3, padx=0, pady=0, sticky="nsew")
+#
+#
+#
+#
+#
+
