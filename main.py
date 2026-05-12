d274af74bc7431bf639beae73bc29361199f7528
FakeLads <22166@students.mrgs.school.nz>
2026-05-12 12:35:00 +1200

    created pycharm file imported tkinter added canvas widgets fixed added widget code window canva & entry widgets inner frame included button and entry dropdown menu exitbutton dropdown id collection

--- main.py
+++ main.py
@@ -13,10 +13,37 @@ root = Tk()
 root.title("Take Credit") #Title of the program window
 root.geometry("1920x1080") # Sets the width and height of the window
 
+id_list = []
+
+branch_list = []
+
+def exit_gui():
+    root.destroy()
+
+
 
 # The 'Loginpage' class holds all functions, methods and attributes related to the first opening page
 class Loginpage:
     def __init__(self, parent):
+
+        def name_collection():
+
+            name = self.entry_id.get().strip()
+
+            if not name:
+                messagebox.showerror("ERROR",
+                                     "Please enter a username")
+                return
+
+            if len(name) > 10:
+                messagebox.showerror("ERROR",
+                                     "Username must be 10 characters or less")
+                return
+
+            else:
+                id_list.append(name)
+
+
 
         self.bg = Image.open("login_page_NO_buttons.png")
 
@@ -29,18 +56,21 @@ class Loginpage:
         self.my_label.image=self.bg
         self.my_label.place(x=0, y=0, relwidth=1, relheight=1)
 
-        self.my_frame = Frame(parent, height=300, width=1000, bg="Red")
+        self.my_frame = Frame(parent, height=300, width=1000, bg="#52005b")
         self.my_frame.place(x=0, y=0, relx=0.5, rely=0.6, anchor=CENTER)
 
         self.entry_id = Entry(self.my_frame, font=("Helvitica", 28))
-        self.entry_id.place(x=50, y=50, anchor = "nw")
+        self.entry_id.grid(row=1, column=1, padx=20, pady=20, columnspan=2)
 
         choices = ['1', '2', '3']
         self.branch_dropdown = ttk.Combobox(self.my_frame, font=("Helvitica", 28), values=choices)
-        self.branch_dropdown.place(x=50, y=50, anchor="ne")
+        self.branch_dropdown.grid(row=1, column=3, padx= 20, pady=20, columnspan=2)
 
         self.button_go = Button(self.my_frame, text="START HERE", height=2, width=16, font=("Helvitica", 20))
-        self.button_go.place(relx=0.5, rely=0.5, anchor = CENTER)
+        self.button_go.grid(row=2, column=2, pady=20, columnspan=2)
+
+        self.button_exit = Button(parent, text="EXIT", height=2, width=7, font=("Helvitica", 20), command=exit_gui)
+        self.button_exit.place(relx=1, rely=1, x=-20, y=-20, anchor="se")
 
 
 
@@ -49,5 +79,10 @@ class Loginpage:
 app = Loginpage(root)
 
 root.mainloop()
+
+
+
+
+
 
 
